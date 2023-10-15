5bf1275fea311661a9bbdf17693cd6c078e6f4b6
jwklo <124646783+jwklo@users.noreply.github.com>
2023-10-15 22:38:04 +0800

    update page.tsx for including demos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,41 @@
-import { Metadata } from 'next';
+'use client';
+//import { Metadata } from 'next';
 import { FaceDetection, FileInputForm } from './client';
 import { DemoToBeImproved } from '@/samples/DemoToBeImproved';
+import { Button } from '@/components/ui/button';
+import { useState } from 'react';
 
 
-export const metadata: Metadata = {
+// export const metadata: Metadata = {
 
-}
-export default function Home() {
-  /*
-  return (
-    <main className="container space-y-4 py-4">
+// }
+
+function content() {
+  const [showdemo, setShowdemo] = useState(false);
+  function showDemoHandler(e: any, isDemo: any) {
+    setShowdemo(isDemo);
+  }
+  if (showdemo) {
+    return <main className="container space-y-4 py-4">
+      <div>
+        <Button onClick={(e) => { showDemoHandler(e, false) }}>FileUpload</Button>
+      </div>
+
       <DemoToBeImproved></DemoToBeImproved>
     </main>
+  } else {
+    return <main className="container space-y-4 py-4">
+      <div>
+        <Button onClick={(e) => { showDemoHandler(e, true) }}>Demo</Button>
+      </div>
 
-  )
-  */
-  
-  return (
-    <main className="container space-y-4 py-4">
       <FileInputForm />
       <FaceDetection />
     </main>
-  );
-  
+  }
+
+}
+
+export default function Home() {
+  return <>{content()}</>
 }

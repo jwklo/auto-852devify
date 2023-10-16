f372928bcf5c391ae062b4086bae4fc49d735f14
wklo <wklo@hko.hksarg>
2023-10-16 18:33:20 +0800

    Signed-off-by: wklo <wklo@hko.hksarg>

--- src/settings/masks.ts
+++ src/settings/masks.ts
@@ -1,5 +1,5 @@
 export const masks = {
-    noggles1: {widthAdjust: 1.4, uri: './red160px.png', comment: "Width Adjust: noggles width(150), noggles holder(30),due expand 1/5 for better output"},
-    noggles2: {widthAdjust: 1.4, uri: './grey-light160px.png'},
-    noggles3: {widthAdjust: 2.3, uri: './frog-green160px.png'},
+    noggles1: {widthAdjust: 1.23, uri: './red160px.png', comment: "Width Adjust: noggles width(150), noggles holder(30),due expand 1/5 for better output"},
+    noggles2: {widthAdjust: 1.23, uri: './grey-light160px.png'},
+    noggles3: {widthAdjust: 1.23, uri: './frog-green160px.png'},
 }
